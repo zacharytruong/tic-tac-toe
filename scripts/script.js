6513445023ef34ec2542c65d4760f9ee2a692d7d
Zachary Truong <zacharytruong@gmail.com>
2021-11-30 22:49:15 -0600

    scripting

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,55 @@
-( function () {
-  const gameboard = {
-    cacheDOm: function (){
-      const gameboard = document.getElementsByClassName("gameboard");
-    },
-    gameboard: [],
+const gameboard = ( function () {
+  
+  const gameboard = [];
+  const cell = (status, spanClass, cellClass) => {
+    this.status = status;
+    return {
+      status,
+    }
+  }
+  // cell.prototype.createCell = () => {
+  //   const span = document.createElement("span");
+  //   span.classList.add(spanClass);
+  //   const div = document.createElement("div");
+  //   div.classList.add(cellClass);
+  //   div.appendChild(span);
+  // };
+  _createAllCells();
+  
+  
+  // Cache DOM
+  const newGame = document.getElementById("new-game");
+  const gameCells = Array.from(document.getElementsByClassName("game-cell"));
+  
+  // Bind events
+  gameCells.forEach( cell => {
+    cell.addEventListener("click", setMark)
+  })
+  newGame.addEventListener("click", createNewGame);
+
+  function _createAllCells () {
+    for (let i = 1; i < 10; i++) {
+      const gameCell = cell(false, "span", "div");
+      gameboard.push(gameCell);
+    }
+    console.log(gameboard);
+  }
+  function setMark () {
+
+  }
+  function createNewGame () {
     
-  };
+  }
+  function _render () {
+
+  }
+  function init () {
+
+  }
+
+  return {
+    createNewGame,
+    setMark,
+    init,
+  }
 })();
