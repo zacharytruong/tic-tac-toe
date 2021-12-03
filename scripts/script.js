08e4b7e2406bdd31526451ed4d8dce387a0ad8b8
Zachary Truong <zacharytruong@gmail.com>
2021-12-03 11:04:02 -0600

    scripting

--- scripts/script.js
+++ scripts/script.js
@@ -11,15 +11,39 @@ const gameboard = ( function () {
       content,
     }
   }
+  const Player = (name, curentTurn, isWinner) => {
+    this.name = name;
+    this.curentTurn = curentTurn;
+    this.isWinner = isWinner;
+    return {
+      name,
+      curentTurn,
+      isWinner,
+    }
+  }
+  const player = Player("Player", true, false);
+  const computer = Player("Computer", false, false);
 
   // Cache Dom
   const gameboard = document.getElementById("gameboard");
   const newGame = document.getElementById("new-game");
-
+  const info = document.getElementById("info");
+  
   // Bind events
   newGame.addEventListener("click", _reset);
+  function _bindGameGrid () {
+    let gameCells = _getGameGrid();
+    gameCells.forEach(gameCell => {
+      gameCell.addEventListener("click", _gamePlay);
+    })
+  }
 
   // Functions
+  function _init () {
+    _displayInfo();
+    _createGameboardTiles(10);
+    _render();
+  }
   function _createNewTileDiv () {
     const span = document.createElement("span");
     span.classList.add("marker");
@@ -34,13 +58,17 @@ const gameboard = ( function () {
       GAMEBOARD.push(gameTile);
     }
   }
+  function _getGameGrid () {
+    const gameCells = Array.from(document.getElementsByClassName("game-cell"));
+    return gameCells;
+  }
   function _removeGameTiles (parent) {
     while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
     }
   }
   function _render () {
-    GAMEBOARD.forEach(gameTile => {
+    GAMEBOARD.forEach( () => {
       gameboard.appendChild(_createNewTileDiv());
     })
   }
@@ -48,25 +76,31 @@ const gameboard = ( function () {
     _removeGameTiles(gameboard);
     _render();
   }
-  function _init () {
-    _createGameboardTiles(10);
-    _render();
+  function _displayInfo () {
+    if (player.curentTurn){
+      info.innerText =`${player.name}'s turn.`;
+    } else {
+      info.innerText = `${computer.name}'s turn.`;
+    }
   }
-  
+  function _switchTurn () {
+    if (player.curentTurn){
+      player.curentTurn = false;
+    } else {
+      player.curentTurn = true;
+    }
+    if (computer.curentTurn){
+      computer.curentTurn = false;
+    } else {
+      computer.curentTurn = true;
+    }
+  }
+  function _gamePlay () {
+    _switchTurn();
+    _displayInfo();
+  }
+
+  // Execution
   _init();
-})();
-
-const gameFlow = ( function () {
-  // Cache Dom
-  const gameCells = Array.from(document.getElementsByClassName("game-cell"));
-  
-  // Bind Events
-  gameCells.forEach(gameCell => {
-    gameCell.addEventListener("click", gamePlay);
-  })
-
-  // Functions
-  function gamePlay () {
-    
-  }
+  _bindGameGrid();
 })();
