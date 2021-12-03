77a1ed7eb3a1386193f7d968830abfc441619a6c
Zachary Truong <zacharytruong@gmail.com>
2021-12-02 22:19:43 -0600

    scripting

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,72 @@
-const game = ( function () {
+const gameboard = ( function () {
 
-  const playArea = [];
-  // const tile = () => {
-  //   const createNewTile = () => {
-  //     const span = document.createElement("span");
-  //     span.classList.add("marker");
-  //     const div = document.createElement("div");
-  //     div.classList.add("game-cell");
-  //     div.appendChild(span);
-  //   }
-  //   return {createNewTile};
-  // }
-  // const gameTileFactory = (status, owner) => {
-  //   const prototype = tile();
-  //   this.status = status;
-  //   this.owner = owner;
-  //   return Object.assign(
-  //     {},
-  //     this.status,
-  //     prototype,
-  //   )
-  // }
-  const Person = (name) => {
-    const sayName = () => console.log(name)
-    return {sayName};
+  const GAMEBOARD = [];
+  const tileFactory = (status, owner, content) => {
+    this.status = status;
+    this.owner = owner;
+    this.content = content;
+    return {
+      status,
+      owner,
+      content,
+    }
   }
-  const Nerd = (name, status) => {
-    let obj = Object.create(Person(name));
-    obj.status = status;
-    return {obj, status}
-  }
+
   // Cache Dom
   const gameboard = document.getElementById("gameboard");
-  
-  
-  const Jeff = Nerd("Jeff", false);
-  // const George = Nerd("George");
-  console.log(Jeff);
+  const newGame = document.getElementById("new-game");
 
+  // Bind events
+  newGame.addEventListener("click", _reset);
+
+  // Functions
+  function _createNewTileDiv () {
+    const span = document.createElement("span");
+    span.classList.add("marker");
+    const div = document.createElement("div");
+    div.classList.add("game-cell");
+    div.appendChild(span);
+    return div;
+  }
+  function _createGameboardTiles (num) {
+    for (let i = 1; i < num; i++){
+      let gameTile = tileFactory(false, "", "");
+      GAMEBOARD.push(gameTile);
+    }
+  }
+  function _removeGameTiles (parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+  function _render () {
+    GAMEBOARD.forEach(gameTile => {
+      gameboard.appendChild(_createNewTileDiv());
+    })
+  }
+  function _reset () {
+    _removeGameTiles(gameboard);
+    _render();
+  }
+  function _init () {
+    _createGameboardTiles(10);
+    _render();
+  }
+  
+  _init();
+})();
+
+const gameFlow = ( function () {
+  // Cache Dom
+  const gameCells = Array.from(document.getElementsByClassName("game-cell"));
+  
+  // Bind Events
+  gameCells.forEach(gameCell => {
+    gameCell.addEventListener("click", gamePlay);
+  })
+
+  // Functions
+  function gamePlay () {
+    
+  }
 })();
