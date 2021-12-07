54306a7d60d717433ddbee59930ff0f3ddf6604a
Zachary Truong <zacharytruong@gmail.com>
2021-12-06 23:37:11 -0600

    scripting

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 const gameboard = ( function () {
-
+  
+  // Setup arrays
   const GAMEBOARD = [];
   const playersArray = [];
+
+  // Setup factories
   const Tile = (status, owner, content, div) => {
     this.status = status;
     this.owner = owner;
@@ -13,21 +16,25 @@ const gameboard = ( function () {
       content,
     }
   }
-  const Player = (name, curentTurn, isWinner, marker) => {
+  const Player = (name, currentTurn, isWinner, marker, ownLand) => {
     this.name = name;
-    this.curentTurn = curentTurn;
+    this.currentTurn = currentTurn;
     this.isWinner = isWinner;
     this.marker = marker;
+    this.ownLand = ownLand;
     return {
       name,
-      curentTurn,
+      currentTurn,
       isWinner,
       marker,
+      ownLand,
     }
   }
-  const player = Player("Player", true, false, "X");
+
+  // Generate players
+  const player = Player("Player", true, false, "X", "");
   playersArray.push(player);
-  const computer = Player("Computer", false, false, "O");
+  const computer = Player("Computer", false, false, "O", "");
   playersArray.push(computer);
 
   // Cache Dom
@@ -41,7 +48,6 @@ const gameboard = ( function () {
 
   // Functions
   function _init () {
-    _displayInfo();
     _createGameboardTiles(10);
     _render();
   }
@@ -51,6 +57,8 @@ const gameboard = ( function () {
       GAMEBOARD.push(gameTile);
     }
   }
+
+  // Only _render() touches DOM
   function _render () {
     let template = "";
     GAMEBOARD.forEach( gameboardTile => {
@@ -58,73 +66,117 @@ const gameboard = ( function () {
       template += gameboardTile.div;
     });
     gameboard.innerHTML = template;
+    let currentWinner = playersArray.find(el => el.isWinner === true);
+    let currentPlayer = playersArray.find(el => el.currentTurn === true);
+    let availableSpace = GAMEBOARD.some(gameTile => gameTile.status === "vacant");
+    if (currentWinner){
+      info.innerText = `${currentWinner.name} has won!`;
+    } else if (!availableSpace){
+      info.innerText = `It's a tie.`;
+    } else if (currentPlayer){
+      info.innerText = `${currentPlayer.name}'s turn.`;
+    }
   }
   function _reset () {
-    player.curentTurn = true;
-    computer.curentTurn = false;
+    player.currentTurn = true;
+    computer.currentTurn = false;
+    GAMEBOARD.forEach( gameTile => {
+      gameTile.status = "vacant";
+      gameTile.owner = "";
+      gameTile.content = "";
+      gameTile.div = "";
+    })
+    playersArray.forEach( gamer => {
+      gamer.isWinner = false;
+      gamer.ownLand = "";
+    })
     _render();
   }
-  function _displayInfo () {
-    if (player.curentTurn){
-      info.innerText =`${player.name}'s turn.`;
-    } else {
-      info.innerText = `${computer.name}'s turn.`;
-    }
-  }
   function _switchTurn () {
-    if (player.curentTurn){
-      player.curentTurn = false;
+    if (player.currentTurn){
+      player.currentTurn = false;
     } else {
-      player.curentTurn = true;
+      player.currentTurn = true;
     }
-    if (computer.curentTurn){
-      computer.curentTurn = false;
+    if (computer.currentTurn){
+      computer.currentTurn = false;
     } else {
-      computer.curentTurn = true;
+      computer.currentTurn = true;
     }
   }
   function _changeOwner (obj, idx) {
     GAMEBOARD[idx].content = obj.marker;
     GAMEBOARD[idx].owner = obj.name;
     GAMEBOARD[idx].status = "occupied";
+    obj.ownLand += idx.toString();
   }
   function _playGame (e) {
-    let list = Array.from(gameboard.querySelectorAll(".game-cell"));
     
-    // Player's turn
-    if (!player.curentTurn){
+    if (playersArray.some(gamer => gamer.isWinner)){
+      _render();
       return;
     } else {
-      let gamecell = e.target.closest(".game-cell");
-      let idx = list.indexOf(gamecell);
-      if (GAMEBOARD[idx].status === "occupied") {
+      let list = Array.from(gameboard.querySelectorAll(".game-cell"));
+
+      // Player's turn
+      if (!player.currentTurn){
+        return;
+      } else if (!GAMEBOARD.some(gameTile => gameTile.status === "vacant")) {
+        _render();
         return;
       } else {
-        _changeOwner(player, idx);
-      }
-      _render();
-    }
-    
-    // Computer's turn
-    _switchTurn();
-    _displayInfo();
-    setTimeout(_computerPlay, 300);
-
-    function _computerPlay () {
-      let randomNum = Math.floor(Math.random() * GAMEBOARD.length);
-      while (GAMEBOARD.some(gameTile => gameTile.status === "vacant") &&
-        GAMEBOARD[randomNum].status === "occupied"){
-          randomNum = Math.floor(Math.random() * GAMEBOARD.length);
+        let gamecell = e.target.closest(".game-cell");
+        let idx = list.indexOf(gamecell);
+        if (GAMEBOARD[idx].status === "occupied") {
+          return;
+        } else {
+          _changeOwner(player, idx);
         }
-      _changeOwner(computer, randomNum);
+        _validateWinner (playersArray);
+        _render();
+      }
+      
+      // Computer's turn
       _switchTurn();
-      _displayInfo();
-      _render();
+      if (_validateWinner (playersArray)){
+        _render();
+        return;
+      } else {
+        setTimeout(_computerPlay, 100);
+      }
+      function _computerPlay () {
+        let randomNum = Math.floor(Math.random() * GAMEBOARD.length);
+        while (GAMEBOARD.some(gameTile => gameTile.status === "vacant") &&
+          GAMEBOARD[randomNum].status === "occupied"){
+            randomNum = Math.floor(Math.random() * GAMEBOARD.length);
+          }
+        _changeOwner(computer, randomNum);
+        _switchTurn();
+        _render();
+        console.table(playersArray)
+      }
     }
   }
     
   // Checkinf for winner
-  
+  function _validateWinner (arr) {
+    const winner = arr.find( el => {
+      return el.ownLand.split("").sort().join("").includes("012") ||
+            el.ownLand.split("").sort().join("").includes("345") ||
+            el.ownLand.split("").sort().join("").includes("678") ||
+            el.ownLand.split("").sort().join("").includes("036") ||
+            el.ownLand.split("").sort().join("").includes("147") ||
+            el.ownLand.split("").sort().join("").includes("025") ||
+            el.ownLand.split("").sort().join("").includes("048") ||
+            el.ownLand.split("").sort().join("").includes("246")
+    });
+    if (winner){
+      winner.isWinner = true;
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   // Execution
   _init();
